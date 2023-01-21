6ef2f9252d91bcb5535ed9365b6c2888c0baec19
Koushik Roy <koushikroy@Koushiks-MacBook-Air.local>
2023-01-21 23:40:44 +0530

    about added

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+// create a route for /about
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
 // listen for requests
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
